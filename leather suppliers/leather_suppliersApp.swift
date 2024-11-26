3d6fc9a2fc7c0a8e6743f65d022ec8cc22696e74
dsv <zxcv>
2024-11-26 23:25:20 +0500

    Initial Commit

---
+++ leather suppliers/leather_suppliersApp.swift
@@ -0,0 +1,17 @@
+//
+//  leather_suppliersApp.swift
+//  leather suppliers
+//
+//  Created by Muhammad Rizwan on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct leather_suppliersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
